3ff81ff616312074393f02951651a0a350fa6967
Suh-Edmond <suhedmond25@yahoo.com>
2026-03-13 12:03:47 +0100

    Add interceptor for requests

---
+++ src/app/interceptors/apiInterceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environments } from '../../environments/environments';
+
+export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const apiReq = req.clone({
+    url: `${environments.apiUrl}${req.url}`,
+    setHeaders: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  });
+
+  return next(apiReq);
+};
