e4ffb8749d1ed0c5d171ead82a5594b1e047843b
Suh-Edmond <suhedmond25@yahoo.com>
2026-03-13 11:40:39 +0100

    added app navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/nav/navbar/navbar.component';
+import { FeePaymentComponent } from './components/fee-payment/fee-payment.component';
+import { StudentAccountComponent } from './components/account/student-account/student-account.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FeePaymentComponent,
+    StudentAccountComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'fee-payment-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FeePaymentComponent } from './components/fee-payment/fee-payment.component';
+import { StudentAccountComponent } from './components/account/student-account/student-account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', redirectTo: '/make-fee-payment', pathMatch: 'full' },
+   { path: 'make-fee-payment', component: FeePaymentComponent, title: 'Institutional Fee Payment' },
+   { path: 'create-student-account', component: StudentAccountComponent, title: 'Student Account' },
+];
